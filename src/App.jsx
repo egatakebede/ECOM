a5700d96c783e97bff90f91e9565af4944c67aff
egatakebede <egatakebede7@gmail.com>
2026-07-17 08:43:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SignupForm from "./components/SignUp";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <SignupForm />
+    </div>
+  );
+};
+
+export default App;
